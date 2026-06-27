947fedb08cdd33f7598e98b89c38422c657639d0
kyura1228 <184086709+kyura1228@users.noreply.github.com>
2026-06-27 16:58:12 +0900

    古いcssの削除、スマホ等の端末でのui修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ function Header({ onPageChange }) {
   return (
     <header>
       <h1 className="name">ShironekotaiMarisa</h1>
-      <div className="header-container" />
-      <button className="header-link" onClick={() => handleNavClick('main')}>
-        メインページ
-      </button>
-      <button className="header-link" onClick={() => handleNavClick('calculator')}>
-        電卓
-      </button>
-      <button className="header-link" onClick={() => handleNavClick('stamp')}>
-        スタンプ
-      </button>
-      <div className="header-container" />
+      <div className="header-actions">
+        <button className="header-link" onClick={() => handleNavClick('main')}>
+          メインページ
+        </button>
+        <button className="header-link" onClick={() => handleNavClick('calculator')}>
+          電卓
+        </button>
+        <button className="header-link" onClick={() => handleNavClick('stamp')}>
+          スタンプ
+        </button>
+      </div>
     </header>
   );
 }
