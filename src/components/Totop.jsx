e624f36edc1db2b6a49149ce640d08aa345f442a
kyura1228 <184086709+kyura1228@users.noreply.github.com>
2026-09-06 15:56:55 +0900

    fix totopを押した際開いているのがmainpageでなくてもmaipageのトップに飛ばされてしまう問題。

--- src/components/Totop.jsx
+++ src/components/Totop.jsx
@@ -1,7 +1,12 @@
 function Totop() {
+    const handleClick = (event) => {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <div className="toTopContainer">
-        <a href="#" className="toTop">^</a>
+        <a href="#top" className="toTop" onClick={handleClick}>^</a>
         </div>
         )};
 
